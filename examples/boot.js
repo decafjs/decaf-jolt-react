9342191e1211d8904a4bfaada849d3d322510bed
Michael Schwartz <mykesx@gmail.com>
2014-10-07 15:45:56 -0700

    debug

--- examples/boot.js
+++ examples/boot.js
@@ -5,7 +5,9 @@ var Application = require('decaf-jolt').Application,
     app = new Application();
 
 app.verb('/', new StaticFile('index.html'));
+app.verb('bower_components', new StaticServer('bower_components'));
 app.verb('hello', new StaticFile('helloworld.html'));
+app.verb('hello2', new StaticFile('hello2.html'));
 app.verb('jsx', new ReactServer('jsx'));
 
 app.listen(9090, '0.0.0.0');
